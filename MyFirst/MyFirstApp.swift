0a46a4b53e642e94e53be42ab9dad78a5515ea23
Josiah <josiah.pilon@gmail.com>
2023-04-04 15:53:11 -0700

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Josiah Pilon on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
